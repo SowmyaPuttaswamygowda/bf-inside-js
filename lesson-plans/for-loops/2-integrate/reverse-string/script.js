13c8a9077b79124dd3ef81f45064104ff11260ec
Sowmya Puttaswamygowda <cpsowmya9@gmil.com>
2024-03-06 14:09:56 +0100

    add for-loop

--- lesson-plans/for-loops/2-integrate/reverse-string/script.js
+++ lesson-plans/for-loops/2-integrate/reverse-string/script.js
@@ -1,11 +1,16 @@
 import { readString, display } from '../../../../lib/dom-io.js';
 
-document.getElementById(__).addEventListener('click', () => {
+document.getElementById("reverse-it").addEventListener('click', () => {
   debugger;
 
   // read user text
+  const userInput = readString("user-text");
 
   // use a for loop that counts down (i--) to reverse the input
-
+ let reversedString = "";
+ for(let i = userInput.length - 1; i >=0; i--) {
+  reversedString += userInput[i];
+ }
   // display the reversed string
+  display("reversed-output",reversedString);
 });
--- lesson-plans/for-loops/2-integrate/upside-down-pyramid/script.js
+++ lesson-plans/for-loops/2-integrate/upside-down-pyramid/script.js
@@ -1,15 +1,35 @@
 import { readString, display } from '../../../../lib/dom-io.js';
 
-document.getElementById(__).addEventListener('click', () => {
-  debugger;
+document.getElementById("pyramid-it").addEventListener('click', () => {
+  //debugger;
 
   // read user text
+  //const userInput = document.getElementById("to-pyramid").value;
 
   // create the pyramid
+  /*const numRows = userInput.length 
 
   for (_; _; _) {
     for (_; _; _) {}
   }
 
   // display the pyramid
+});*/
+// read user text
+debugger;
+  const userInput = document.getElementById("to-pyramid").value;
+
+  // create the pyramid
+  const numRows = userInput.length;
+
+  let pyramid = "";
+  for (let i = numRows; i >= 1; i--) {
+    // Add spaces for indentation
+    pyramid += " ".repeat(numRows - i);
+    // Add characters for the pyramid
+    pyramid += userInput.slice(0, i) + "\n";
+  }
+
+  // display the pyramid
+  display( "pyramided",pyramid);
 });
